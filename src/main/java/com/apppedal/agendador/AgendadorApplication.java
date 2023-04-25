5c1f8e071346ff14d3ad254c4642e087e3c5e5ce
vitimon <leonardongc@gmail.com>
2023-04-25 15:19:13 -0300

    reseted all, dropped JSF :(

---
+++ src/main/java/com/apppedal/agendador/AgendadorApplication.java
@@ -0,0 +1,13 @@
+package com.apppedal.agendador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendadorApplication.class, args);
+	}
+
+}
